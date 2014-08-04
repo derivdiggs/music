15a2ad7c5ef33b97a6cdca319962c1cfd7ad6e37
derevdiggs <git@thoughtrender.com>
2014-08-04 17:11:30 -0400

    initial commit for uploading
    Keeping code for uploading separate from main code

---
+++ upload/assets/js/main.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+var knob = require('./jquery.knob.js');
+var widget = require('./jquery.ui.widget.js');
+var script = require('./script.js');
+var iframe = require('./jquery.iframe-transport.js');
+var fileupload = require('./jquery.fileupload.js');
